1a79010fdc12cbece8e88065d8365a86e943b69f
nihal09 <nihal.s@udaan.com>
2023-08-31 05:28:29 +0530

    Created console

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BusinessProfileForm from './components/BusinessProfileForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/user/:userId/profile/:profileId">
+              {({ match }) => {
+                  const { userId, profileId } = match!=null ? match.params : {userId :"6a291a13-3a3f-4cf1-b51d-85e0fa8d8166", profileId:"64e5b94a-e3d3-40ab-8aa5-4e39789e37b7"};
+                return <BusinessProfileForm userId={userId} profileId={profileId} />;
+              }}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
